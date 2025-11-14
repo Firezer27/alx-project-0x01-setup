ca74612779a8ba9165370eef5995480c38df011b
Firezer27 <firezer.bizuwork27@gmail.com>
2025-11-14 04:30:14 +0300

    update alx-project-0x01 file

---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/layout/Header";
+
+const UsersPage: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <div className="p-10">
+        <h1 className="text-4xl font-thin">Users Page</h1>
+      </div>
+    </div>
+  );
+};
+
+export default UsersPage;
